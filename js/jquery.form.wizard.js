5a5822eb9f6e3aabd42ba4f02ece907737911158
Matthew Johnston <warmwaffles@gmail.com>
2011-02-21 01:30:40 -0600

    Removed alert's. Should be using console.log instead

--- js/jquery.form.wizard.js
+++ js/jquery.form.wizard.js
@@ -3,12 +3,12 @@
  *
  *
  * Copyright (c) 2010 Jan Sundman (jan.sundman[at]aland.net)
- * 
+ *
  * http://www.thecodemine.org
  *
  * Licensed under the MIT licens:
  *   http://www.opensource.org/licenses/mit-license.php
- * 
+ *
  */
 
 
@@ -16,15 +16,15 @@
 	$.widget("ui.formwizard", {
 
 		_init: function() {
-			
+
 			var wizard = this;
 			var formOptionsSuccess = this.options.formOptions.success;
-			var formOptionsComplete = this.options.formOptions.complete;	
-			var formOptionsBeforeSend = this.options.formOptions.beforeSend; 
-			var formOptionsBeforeSubmit = this.options.formOptions.beforeSubmit; 
-			var formOptionsBeforeSerialize = this.options.formOptions.beforeSerialize; 
+			var formOptionsComplete = this.options.formOptions.complete;
+			var formOptionsBeforeSend = this.options.formOptions.beforeSend;
+			var formOptionsBeforeSubmit = this.options.formOptions.beforeSubmit;
+			var formOptionsBeforeSerialize = this.options.formOptions.beforeSerialize;
 			this.options.formOptions = $.extend(this.options.formOptions,{
-				success	: function(responseText, textStatus, xhr){ 
+				success	: function(responseText, textStatus, xhr){
 					if(formOptionsSuccess){
 						formOptionsSuccess(responseText, textStatus, xhr);
 					}
@@ -54,23 +54,23 @@
 						return shouldSubmit;
 					}
 				},
-				beforeSerialize: function(form, options) { 
+				beforeSerialize: function(form, options) {
 					if(formOptionsBeforeSerialize){
 						var shouldSubmit = formOptionsBeforeSerialize(form, options);
 						if(!shouldSubmit)
 							wizard._enableNavigation();
 						return shouldSubmit;
-					}				    
+					}
 				}
 			});
-			
+
 			this.steps = this.element.find(".step").hide();
-			
+
 			this.firstStep = this.steps.eq(0).attr("id");
 			this.activatedSteps = new Array();
 			this.isLastStep = false;
 			this.previousStep = undefined;
-			this.currentStep = this.steps.eq(0).attr("id");		
+			this.currentStep = this.steps.eq(0).attr("id");
 			this.nextButton	= this.element.find(this.options.next)
 					.click(function() {
 						return wizard._next();
@@ -81,7 +81,7 @@
 
     			this.backButton	= this.element.find(this.options.back)
     				.click(function() {
-    					wizard._back();return false; 
+    					wizard._back();return false;
     				});
 
 	    		this.backButtonInitinalValue = this.backButton.val();
@@ -89,19 +89,19 @@
 
 			if(this.options.validationEnabled && jQuery().validate  == undefined){
 				this.options.validationEnabled = false;
-				alert("the validation plugin needs to be included");
+				console.log("%s", "the validation plugin needs to be included");
 			}else if(this.options.validationEnabled){
 				this.element.validate(this.options.validationOptions);
 			}
 			if(this.options.formPluginEnabled && jQuery().ajaxSubmit == undefined){
 				this.options.formPluginEnabled = false;
-				alert("the form plugin needs to be included");
+				console.log("%s", "the form plugin needs to be included");
 			}
-	
+
 			if(this.options.disableInputFields == true){
 				$(this.steps).find(":input:not('.wizard-ignore')").attr("disabled","disabled");
 			}
-			
+
 			if(this.options.historyEnabled){
 				$(window).bind('hashchange', undefined, function(event){
 					var hashStep = event.getState( "_" + $(wizard.element).attr( 'id' )) || wizard.firstStep;
@@ -110,7 +110,7 @@
 							if(!wizard.element.valid()){
 								wizard._updateHistory(wizard.currentStep);
 								wizard.element.validate().focusInvalid();
-								
+
 								return false;
 							}
 						}
@@ -121,14 +121,14 @@
 			}
 
 			this.element.addClass("ui-formwizard");
-			this.element.find(":input").addClass("ui-wizard-content");	
+			this.element.find(":input").addClass("ui-wizard-content");
 			this.steps.addClass("ui-formwizard-content");
 			this.backButton.addClass("ui-formwizard-button ui-wizard-content");
 			this.nextButton.addClass("ui-formwizard-button ui-wizard-content");
-			
+
 			if(!this.options.disableUIStyles){
 				this.element.addClass("ui-helper-reset ui-widget ui-widget-content ui-helper-reset ui-corner-all");
-				this.element.find(":input").addClass("ui-helper-reset ui-state-default");	
+				this.element.find(":input").addClass("ui-helper-reset ui-state-default");
 				this.steps.addClass("ui-helper-reset ui-corner-all");
 				this.backButton.addClass("ui-helper-reset ui-state-default");
 				this.nextButton.addClass("ui-helper-reset ui-state-default");
@@ -136,7 +136,7 @@
 			this._show(undefined);
 			return $(this);
 		},
-   
+
 		_next : function(){
 			if(this.options.validationEnabled){
 				if(!this.element.valid()){
@@ -144,25 +144,25 @@
 					return false;
 				}
 			}
-			
-			if(this.options.remoteAjax != undefined){ 
+
+			if(this.options.remoteAjax != undefined){
 				var options = this.options.remoteAjax[this.currentStep];
 				var wizard = this;
-				if(options !== undefined){ 
+				if(options !== undefined){
 					var success = options.success;
 					var beforeSend = options.beforeSend;
 					var complete = options.complete;
-					
+
 					options = $.extend({},options,{
 						success: function(data, statusText){
 							if((success !== undefined && success(data, statusText)) || (success == undefined)){
-								wizard._continueToNextStep();	
+								wizard._continueToNextStep();
 							}
 						},
 						beforeSend : function(xhr){
 							wizard._disableNavigation();
 							if(beforeSend !== undefined)
-								beforeSend(xhr);							
+								beforeSend(xhr);
 							$(wizard.element).trigger('before_remote_ajax', {"currentStep" : wizard.currentStep});
 						},
 						complete : function(xhr, statusText){
@@ -179,32 +179,32 @@
 
 			return this._continueToNextStep();
 		},
-	
+
 		_back : function(){
 			if(this.activatedSteps.length > 0){
 				if(this.options.historyEnabled){
 					this._updateHistory(this.activatedSteps[this.activatedSteps.length - 2]);
 				}else{
-					this._show(this.activatedSteps[this.activatedSteps.length - 2], true);	
+					this._show(this.activatedSteps[this.activatedSteps.length - 2], true);
 				}
 			}
 			return false;
 		},
-	   
+
 		_continueToNextStep : function(){
-			if(this.isLastStep){ 
+			if(this.isLastStep){
 				for(var i = 0; i < this.activatedSteps.length; i++){
 					this.steps.filter("#" + this.activatedSteps[i]).find(":input").not(".wizard-ignore").removeAttr("disabled");
 				}
 				if(!this.options.formPluginEnabled){
 					return true;
-				}else{ 
+				}else{
 					this._disableNavigation();
 					this.element.ajaxSubmit(this.options.formOptions);
 					return false;
 				}
 			}
-			
+
 			var step = this._navigate(this.currentStep);
 			if(step == this.currentStep){
 				return false;
@@ -216,13 +216,13 @@
 			}
 			return false;
 		},
-	
+
 		_updateHistory : function(step){
 			var state = {};
 			state["_" + $(this.element).attr('id')] = step;
 			$.bbq.pushState(state);
 		},
-		
+
 		_disableNavigation : function(){
 			this.nextButton.attr("disabled","disabled");
 			this.backButton.attr("disabled","disabled");
@@ -231,27 +231,27 @@
 				this.backButton.removeClass("ui-state-active").addClass("ui-state-disabled");
 			}
 		},
-	
+
 		_enableNavigation : function(){
 			if(this.isLastStep){
 				this.nextButton.val(this.options.textSubmit);
 			}else{
 				this.nextButton.val(this.options.textNext);
 			}
-		
+
 			if($.trim(this.currentStep) !== this.steps.eq(0).attr("id")){
 				this.backButton.removeAttr("disabled");
 				if(!this.options.disableUIStyles){
 					this.backButton.removeClass("ui-state-disabled").addClass("ui-state-active");
 				}
 			}
-				
+
 			this.nextButton.removeAttr("disabled");
 			if(!this.options.disableUIStyles){
 				this.nextButton.removeClass("ui-state-disabled").addClass("ui-state-active");
 			}
 		},
-		   
+
 		_animate : function(oldStep, newStep, stepShownCallback){
 			this._disableNavigation();
 			var old = this.steps.filter("#" + oldStep);
@@ -270,14 +270,14 @@
 				return;
 			});
 		},
-	
+
 		_checkIflastStep : function(step){
 			this.isLastStep = false;
 			if($("#" + step).hasClass(this.options.submitStepClass) || this.steps.filter(":last").attr("id") == step){
 				this.isLastStep = true;
 			}
 		},
-	   
+
 		_getLink : function(step){
 			var link = undefined;
 			var links = this.steps.filter("#" + step).find(this.options.linkClass);
@@ -286,33 +286,33 @@
 				if(links.filter(":radio,:checkbox").size() > 0){
 					link = links.filter(this.options.linkClass + ":checked").val();
 				}else{
-					link = $(links).val();				
+					link = $(links).val();
 				}
 			}
 			return link;
 		},
-	   
+
 		_navigate : function(step){
 			var link = this._getLink(step);
 			if(link != undefined){
 				if((link != "" && link != null && link != undefined) && this.steps.filter("#" + link).attr("id") != undefined){
 					return link;
 				}
-				return this.currentStep;				
-			}else if(link == undefined && !this.isLastStep){	
+				return this.currentStep;
+			}else if(link == undefined && !this.isLastStep){
 				var step1 =  this.steps.filter("#" + step).next().attr("id");
 				return step1;
 			}
 		},
-	   
+
 		_show : function(step){
 			var backwards = false;
 			var triggerStepShown = step !== undefined;
-			if(step == undefined || step == ""){ 
+			if(step == undefined || step == ""){
 					this.activatedSteps.pop();
 					step = this.firstStep;
 					this.activatedSteps.push(step);
-			}else{		
+			}else{
 				if($.inArray(step, this.activatedSteps) > -1){
 					backwards = true;
 					this.activatedSteps.pop();
@@ -328,19 +328,19 @@
 				var stepShownCallback = function(){if(triggerStepShown)$(this.element).trigger('step_shown', $.extend({"isBackNavigation" : backwards},this._state()));};
 				this._animate(this.previousStep, step, stepShownCallback);
 			};
-			
-			
+
+
 		},
-	   
+
 	   _reset : function(){
 			this.element.resetForm()
-			$("label,:input,textarea",this).removeClass("error");		
+			$("label,:input,textarea",this).removeClass("error");
 			for(var i = 0; i < this.activatedSteps.length; i++){
 				this.steps.filter("#" + this.activatedSteps[i]).hide().find(":input").attr("disabled","disabled");
 			}
 			this.activatedSteps = new Array();
-			this.previousStep = undefined;	
-			this.isLastStep = false;	
+			this.previousStep = undefined;
+			this.isLastStep = false;
 			if(this.options.historyEnabled){
 				this._updateHistory(this.firstStep);
 			}else{
@@ -361,15 +361,15 @@
 				"steps" : this.steps,
 				"firstStep" : this.firstStep
 			}
-			
+
 			if(state !== undefined)
 				return currentState[state];
-				
+
 			return currentState;
 		},
-	   
+
 	  /*Methods*/
-	  
+
 		show : function(step){
 			if(this.options.historyEnabled){
 				this._updateHistory(step);
@@ -380,20 +380,20 @@
 
 		state : function(state){
 			return this._state(state);
-		},		
+		},
 
 		reset : function(){
 			this._reset();
 		},
-		
+
 		next : function(){
-			this._next();		
+			this._next();
 		},
-		
+
 		back : function(){
 			this._back();
 		},
-		
+
 		destroy: function() {
 			this.element.find("*").removeAttr("disabled").show();
 			this.nextButton.unbind("click").val(this.nextButtonInitinalValue).removeClass("ui-state-disabled").addClass("ui-state-active");
@@ -412,13 +412,13 @@
 			this.steps = undefined;
 			this.firstStep = undefined;
 		},
-		
+
 		update_steps : function(){
 			this.steps = this.element.find(".step").addClass("ui-formwizard-content");
 			this.steps.not("#" + this.currentStep).hide().find(":input").addClass("ui-wizard-content").attr("disabled","disabled");
 			this._checkIflastStep(this.currentStep);
 			this._enableNavigation();
-			if(!this.options.disableUIStyles){			
+			if(!this.options.disableUIStyles){
 				this.steps.addClass("ui-helper-reset ui-corner-all");
 				this.steps.find(":input").addClass("ui-helper-reset ui-state-default");
 			}
@@ -436,7 +436,7 @@
 			textSubmit : 'Submit',
 			textNext : 'Next',
 			textBack : 'Back',
-			remoteAjax : undefined, 
+			remoteAjax : undefined,
 			inAnimation : {opacity: 'show'},
 	        	outAnimation: {opacity: 'hide'},
 			inDuration : 400,
@@ -444,8 +444,8 @@
 			easing: 'swing',
 			focusFirstInput : false,
 			disableInputFields : true,
-			formOptions : { reset: true, success: function(data) { alert("success"); },
-			disableUIStyles : false 
+			formOptions : { reset: true, success: function(data) { console.log("%s", "success"); },
+			disableUIStyles : false
 		}
    }
  });
